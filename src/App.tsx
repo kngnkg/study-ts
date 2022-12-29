fc55d0b94f5e57f7d6cbd81f8ea3f302b896a180
ryokawa <kawata.ryo.0518@gmail.com>
2022-12-29 12:54:02 +0900

    optional chaining

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import './App.css';
 import { Todo } from './Todo';
 import { TodoType } from './types/todo';
 import { Text } from './Text';
+import { UserProfile } from './UserProfile';
+import { User } from './types/user';
+
+const user: User = {
+  name: "kawa",
+  hobbies: ["映画", "漫画"]
+}
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -15,6 +22,7 @@ function App() {
   }
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -5,4 +5,4 @@ export const Todo: FC<Omit<TodoType, "id">> = (props) => {
     const { title, userId, completed = false } = props;
     const completeMark = completed ? "[完]" : "[未]";
     return <p>{`${completeMark} ${title}(ユーザ:${userId})`}</p>
-}
+};
